76407cf85f826fec5d81182ee286ec8f3838d3aa
Erick Garcia <egarciasc87@gmail.com>
2023-10-13 10:10:01 -0500

    v1: set up solution + EntityFramework

---
+++ RunGroupWebApp/RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace RunGroupWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }  
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
